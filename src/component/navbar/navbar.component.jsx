0008c81b4779562109291e199ed8cdebfa404f84
Ha_Kyo <luongthithuyha2612@gmail.com>
2022-11-16 19:14:19 +0700

    fix error in mobile-menu

--- src/component/navbar/navbar.component.jsx
+++ src/component/navbar/navbar.component.jsx
@@ -6,7 +6,6 @@ import {ReactComponent as Logo} from '../../assets/images/logo.svg'
 const Navbar=()=>  {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
     const onClickhandler=()=>{
-        console.log(isNavExpanded)
         return setIsNavExpanded(!isNavExpanded)
     }
 return <div className='navbar-container'>
@@ -14,21 +13,22 @@ return <div className='navbar-container'>
 <div className='mobile-menu' onClick={onClickhandler} >
 <IconMenu />
 </div>
-{isNavExpanded ? <div className='navlinks'>
-    <div className='navlink'>Home </div>
-    <div className='navlink'>New </div>
-    <div className='navlink'>Popular </div>
-    <div className='navlink'>Trending </div>
-    <div className='navlink'>Categories </div>
-</div> : 
-<div className='navlinks expanded'>
+{isNavExpanded ? <div className='navlinks expanded'>
 <div className='mobile-menu-close' onClick={onClickhandler} > <IconMenuClose/> </div>
     <div className='navlink'>Home </div>
     <div className='navlink'>New </div>
     <div className='navlink'>Popular </div>
     <div className='navlink'>Trending </div>
     <div className='navlink'>Categories </div>
-</div>}
+</div>:
+<div className='navlinks'>
+    <div className='navlink'>Home </div>
+    <div className='navlink'>New </div>
+    <div className='navlink'>Popular </div>
+    <div className='navlink'>Trending </div>
+    <div className='navlink'>Categories </div>
+</div> 
+}
 
 </div>
 
